7c8155c3196ca72bc535d0d7c7e338946cceaf72
Haoran <chaoran@umich.edu>
2024-03-24 21:37:42 -0400

    modify GQA_vis.sh

--- GQA_vis.sh
+++ GQA_vis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define an array of method names
-methods=("Gating_kv" "Gating_q" "Gating_q_and_kv")
+methods=("choice_one" "choice_two" "choice_three")
 
 # Backup the original model.py file
 cp model.py model_backup.py
@@ -14,15 +14,21 @@ for method in "${methods[@]}"; do
     cp model_backup.py model.py
 
     # Uncomment the lines for the current method and comment out the others
-    if [ "$method" == "Gating_kv" ]; then
-        sed -i 's/#Gating_kv/Gating_kv/' model.py
-        sed -i 's/Gating_q/#Gating_q/' model.py
-        sed -i 's/Gating_q_and_kv/#Gating_q_and_kv/' model.py
-    elif [ "$method" == "Gating_q" ]; then
+    if [ "$method" == "choice_one" ]; then
+        pattern="#Gating_kv = nn.Linear(self.n_embd, self.kv_dim, bias=True, device=x.device)"
+        file="model.py"
+        # Escape special characters in the pattern
+        escaped_pattern=$(echo "$pattern" | sed 's/[\&/]/\\&/g')
+        # Use sed to remove the first # character from the four lines following the pattern
+        sed -i '' "/$escaped_pattern/{n;s/^#//;n;s/^#//;n;s/^#//;n;s/^#//}" "$file"
+        sed -n '138,142p' model.py
+
+
+    elif [ "$method" == "choice_two" ]; then
         sed -i 's/#Gating_q/Gating_q/' model.py
         sed -i 's/Gating_kv/#Gating_kv/' model.py
         sed -i 's/Gating_q_and_kv/#Gating_q_and_kv/' model.py
-    elif [ "$method" == "Gating_q_and_kv" ]; then
+    elif [ "$method" == "choice_three" ]; then
         sed -i 's/#Gating_q_and_kv/Gating_q_and_kv/' model.py
         sed -i 's/Gating_kv/#Gating_kv/' model.py
         sed -i 's/Gating_q/#Gating_q/' model.py
