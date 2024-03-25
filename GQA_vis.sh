e6a35129b33c4d7524344dec69d48ffcdbdbc8b3
Haoran <chaoran@umich.edu>
2024-03-25 03:39:37 -0400

    print out line 138-142

--- GQA_vis.sh
+++ GQA_vis.sh
@@ -86,7 +86,7 @@ for method in "${methods[@]}"; do
 
     # Run your training script with the modified model.py
     python3 data/shakespeare_char/prepare.py
-    python3 train.py --out_dir=out --device=cpu --block_size=2 --batch_size=2 --n_layer=2 --n_head=2 --n_embd=16 --lr_decay_iters=2 --gate --dtype="float32" --max_iter=100 --no-use_rotary_embeddings --use_abs_pos_embeddings --use_abs_pos_embeddings --no-use_post_ln
+    python3 train.py --out_dir=out --eval_interval=50 --log_interval=1 --device=cpu --block_size=2 --batch_size=2 --n_layer=2 --n_head=2 --n_embd=16 --lr_decay_iters=2 --gate --dtype="float32" --max_iter=100 --no-use_rotary_embeddings --use_abs_pos_embeddings --use_abs_pos_embeddings --no-use_post_ln
 
     python3 weight_vis.py
 done
